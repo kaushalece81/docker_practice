85763dc58375cdd3d1ccc5f0860d9e72bce3aa2e
DESKTOP-TUH6J3I\kaushlender sharma <kaushalece81@gmail.com>
2022-03-10 16:57:20 +0530

    docker first commit

---
+++ hello-world-rest-api/src/test/java/com/example/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.rest.webservices.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class RestfulWebServicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
